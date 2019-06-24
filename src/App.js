cc81550d96a5dadc58246d1561dc1bc5108a82d3
jmarcossf <jamlmarcos@gmail.com>
2019-06-24 03:39:09 -0700

    Added some basic styling.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect} from 'react-redux';
 import { debounce } from 'lodash';
-import { Layout, Menu, Breadcrumb, Modal } from 'antd';
+import { Layout, Menu, Icon, Modal, Avatar } from 'antd';
 import SideTree from './components/SideTree';
 import NodeShow from './components/NodeShow'
 import './App.css';
@@ -38,19 +38,31 @@ class App extends Component {
     {this.props.error && this.error()}
     return (
       <div className="App">
+        <div style={{height: '66px'}}>
+          <Avatar style={{float: 'right'}}size={64} icon="user" />
+        </div>
         <Layout>
           <Header className="header">
             <div className="logo" />
           </Header>
-          <Layout>
+          <Layout style={{ padding: '0 0px 2px' }}>
             <Sider width={'20%'} style={{ background: '#fff' }}>
+                <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
+                  <Menu.Item key="mail">
+                    <Icon type="unordered-list" />
+                    <span></span>
+                  </Menu.Item>
+                  <Menu.Item key="mail">
+                    <Icon type="menu-unfold" />
+                  </Menu.Item>
+                </Menu>
               <SideTree
                 treeData={this.props.treeData}
                 loading={this.props.loading}
                 handleTreeNodeClick={this.handleTreeNodeClick}
               />
             </Sider>
-            <Layout style={{ padding: '0 24px 24px' }}>
+            <Layout style={{ padding: '0 2px 0px' }}>
               <Content
                 style={{
                   background: '#fff',
